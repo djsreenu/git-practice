ab7e71f76d0bc093bd17f6c3eeaca9e990532a31
djsreenu <djsreenu@gmail.com>
2026-02-18 22:51:04 +0530

    variable

---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hi $PERSON2 , how are you"
+echo "$PERSON2 : I am fine, what about you"
+echo "$PERSON1 : I am doing great"
+echo "$PERSON2 : I started learning shell script $PERSON1"
