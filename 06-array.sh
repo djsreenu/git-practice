d5a436fbe4af33aa2b8c3a9076d5a42d67947cee
djsreenu <djsreenu@gmail.com>
2026-02-22 12:06:51 +0530

    array

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "KIWI" "BANANA") ## array
+#index start from 0, size is 3
+
+echo "Frist fruit is : $FRUITS[0]"
+echo "Frist fruit is : $FRUITS[1]"
+echo "Frist fruit is : $FRUITS[2]"
+echo "Frist fruit is : $FRUITS[@]"
