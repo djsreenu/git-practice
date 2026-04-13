0ca0cce9369912a89dcc05084019ad504a7fa4bc
djsreenu <djsreenu@gmail.com>
2026-04-13 12:58:05 +0530

    loops

--- 10-redirectors.sh
+++ 10-redirectors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOG_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOG_FOLDER/$SCRIP_NAME-$TIME_STAMP.log"
 mkdir -p $LOG_FOLDER
